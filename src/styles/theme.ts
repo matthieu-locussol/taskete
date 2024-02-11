a6eb4330b74c8a87afc598eb328c73ffee9d161b
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-02-11 11:35:01 +0900

    style: changed font to MPlusRounded1C

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -40,7 +40,7 @@ export const theme = createTheme({
    typography: (palette) => ({
       allVariants: {
          fontFamily:
-            "NotoSansJP, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
+            "MPlusRounded1C, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
       },
       h1: {
          fontSize: '2rem',
