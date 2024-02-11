b0a6a4147f2ee2b9e23c5ca971948cfdc64dc642
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-02-11 20:58:58 +0900

    refactor: added Timers component

---
+++ src/components/Timers.tsx
@@ -0,0 +1,52 @@
+import { Box, NoSsr, Tab, Tabs, styled, tabsClasses } from '@mui/material';
+import { Card } from '../components/Card';
+import { StartButton } from '../components/StartButton';
+import { Timer } from '../components/Timer';
+import { useStore } from '../store';
+import { PomodoroState } from '../store/PomodoroStore';
+
+export const Timers = () => {
+   const { pomodoroStore } = useStore();
+
+   return (
+      <Box display="flex" flexDirection="column" alignItems="center">
+         <Card maxWidth="sm" sx={{ mt: 2 }}>
+            <StyledTabs
+               value={pomodoroStore.state}
+               onChange={(_, newState: PomodoroState) => pomodoroStore.setState(newState)}
+            >
+               <Tab label="Working" value="working" />
+               <Tab label="Short break" value="shortBreak" />
+               <Tab label="Long break" value="longBreak" />
+            </StyledTabs>
+            <Box display="flex" justifyContent="center" mt={2} mb={4}>
+               <Timer
+                  value={pomodoroStore.remainingSecondsRatio * 100}
+                  label={pomodoroStore.remainingSecondsStr}
+               />
+            </Box>
+            <Box display="flex" justifyContent="center">
+               <NoSsr>
+                  <StartButton
+                     disableRipple
+                     onClick={() =>
+                        pomodoroStore.paused ? pomodoroStore.start() : pomodoroStore.pause()
+                     }
+                     sx={{ m: 'auto' }}
+                  >
+                     {pomodoroStore.paused ? 'Start' : 'Pause'}
+                  </StartButton>
+               </NoSsr>
+            </Box>
+         </Card>
+      </Box>
+   );
+};
+
+const StyledTabs = styled(Tabs)(() => ({
+   [`& .${tabsClasses.flexContainer}`]: {
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+   },
+}));
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
-import { Box, NoSsr, Tab, Tabs, styled, tabsClasses } from '@mui/material';
 import { observer } from 'mobx-react-lite';
 import Head from 'next/head';
-import { Card } from '../components/Card';
 import { Layout } from '../components/Layout';
-import { StartButton } from '../components/StartButton';
-import { Timer } from '../components/Timer';
+import { Timers } from '../components/Timers';
 import { useStore } from '../store';
-import { PomodoroState } from '../store/PomodoroStore';
 
 const Index = observer(() => {
    const { pomodoroStore } = useStore();
@@ -16,47 +12,9 @@ const Index = observer(() => {
          <Head>
             <title>{pomodoroStore.title}</title>
          </Head>
-         <Box display="flex" flexDirection="column" alignItems="center">
-            <Card maxWidth="sm" sx={{ mt: 2 }}>
-               <StyledTabs
-                  value={pomodoroStore.state}
-                  onChange={(_, newState: PomodoroState) => pomodoroStore.setState(newState)}
-               >
-                  <Tab label="Working" value="working" />
-                  <Tab label="Short break" value="shortBreak" />
-                  <Tab label="Long break" value="longBreak" />
-               </StyledTabs>
-               <Box display="flex" justifyContent="center" mt={2} mb={4}>
-                  <Timer
-                     value={pomodoroStore.remainingSecondsRatio * 100}
-                     label={pomodoroStore.remainingSecondsStr}
-                  />
-               </Box>
-               <Box display="flex" justifyContent="center">
-                  <NoSsr>
-                     <StartButton
-                        disableRipple
-                        onClick={() =>
-                           pomodoroStore.paused ? pomodoroStore.start() : pomodoroStore.pause()
-                        }
-                        sx={{ m: 'auto' }}
-                     >
-                        {pomodoroStore.paused ? 'Start' : 'Pause'}
-                     </StartButton>
-                  </NoSsr>
-               </Box>
-            </Card>
-         </Box>
+         <Timers />
       </Layout>
    );
 });
-
-const StyledTabs = styled(Tabs)(() => ({
-   [`& .${tabsClasses.flexContainer}`]: {
-      width: '100%',
-      display: 'flex',
-      justifyContent: 'center',
-   },
-}));
 
 export default Index;
