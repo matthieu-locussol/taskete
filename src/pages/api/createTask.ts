c49bd97aa9c54be99268743cb33bffe89c75ca84
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-02-13 16:55:33 +0900

    fix: removed console.log

--- src/pages/api/createTask.ts
+++ src/pages/api/createTask.ts
@@ -27,7 +27,6 @@ export default async function handler(req: NextRequest) {
       });
    }
 
-   console.log(tags);
    const tagsArray: Tag[] = tags.split('#;#').map((json) => JSON.parse(json));
 
    const task = await prisma.task.create({
